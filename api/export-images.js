bdf3125434df96db53e5206b65589f23f9bc3d81
Gustavo Santos <gutopc@gmail.com>
2025-05-04 21:11:07 -0300

    Primeiro commit Segundo

--- api/export-images.js
+++ api/export-images.js
@@ -5,8 +5,27 @@ export default async function handler(req, res) {
      }
    
      try {
-       // Aqui vai sua lógica de leitura da planilha, download da imagem, envio à AWS etc.
-       res.status(200).json({ success: true, message: 'Imagens exportadas' });
+    const filePath = req.file.path;
+    const imagens = await loadWorkbook(filePath); // nome e buffer
+
+    let count = 0;
+    
+for (const img of imagens) {
+  const command = new PutObjectCommand({
+    Bucket: "moribr",
+    Key: `base-fotos/${img.nomeArquivo}`,
+    Body: img.buffer,
+    ACL: "public-read",
+    ContentType: "image/jpeg",
+  });
+
+  await s3.send(command);
+  console.log(`✔️ Enviado: ${img.nomeArquivo}`);
+  count++;
+}
+    
+
+    fs.unlinkSync(filePath); // remove temp       res.status(200).json({ success: true, message: 'Imagens exportadas' });
      } catch (err) {
        console.error('Erro:', err);
        res.status(500).json({ success: false, message: 'Erro interno do servidor' });
